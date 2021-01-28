ee59035c889572368957d332631f80e8542054b3
Akhror <akhsoliev@gmail.com>
2021-01-28 09:47:38 +0500

    added home and about parts

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import AppHero from '../home/hero'
-
+import About from './about'
+import AppFeature from './feature'
 
 function AppHome() {
   return (
     <div className="main">
       <AppHero />
+      <About />
+      <AppFeature />
     </div>
   )
 }
