ff4224a11fbf91b83f88ce367d4a1ad9c1cde186
Akhror <akhsoliev@gmail.com>
2021-01-29 23:32:35 +0500

    added footer part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import './App.css'
 import 'antd/dist/antd.css'
 import { Layout } from 'antd'
 import AppHeader from './components/common/header'
-import { Content } from 'antd/lib/layout/layout'
+import { Content, Footer } from 'antd/lib/layout/layout'
 import AppHome from './components/views/home'
+import Footer1 from './components/common/footer'
+
 const { Header } = Layout
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
         <Content>
           <AppHome />
         </Content>
+        <Footer>
+          <Footer1 />
+        </Footer>
       </Layout>
     </div>
   )
